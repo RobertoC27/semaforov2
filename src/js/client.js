00d15ef445822f260910729ccb318e6410c7d30a
RobertoC27 <chi13027@uvg.edu.gt>
2016-08-23 21:57:34 -0600

    initial commit

---
+++ src/js/client.js
@@ -0,0 +1,13 @@
+import expect from 'expect';
+import { createStore } from 'redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Immutable from 'immutable';
+
+//import '../sass/style.scss'
+// Javascript the good parts
+
+
+const trafficLight = (state = 0, action) => (
+    
+  );
